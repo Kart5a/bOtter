a1bd82ee2ea7a5eff1988f44affa1cd7477f13f9
Kart5a <kaartovuori.tino@gmail.com>
2018-04-22 17:05:05 +0300

    m

--- server.js
+++ server.js
@@ -175,13 +175,12 @@ const commands = {
             url: linkki,
             title: "PÄÄPÄIVÄ",
             requester: msg.author.username
-          });
-        }).then(() => jatka());
+          }).then(() => soita());
+        });
 
-        function jatka() {
+        function soita() {
         if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
-
-      }};
+      }
 
     } else {
       msg.channel.send("Sulla ei oo oikeuksia määrittää pääpäivää t. bOtter");
