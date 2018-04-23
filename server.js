d09fff96b205af028e2b02e8bb12f235a8d824d4
Kart5a <kaartovuori.tino@gmail.com>
2018-04-23 09:55:05 +0300

    m

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ const commands = {
     // ALKAA SOITTAA QUEUEA //
     function startPlay(msg) {
       console.log("Soitetaan!");
-      if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.satrtPlay(msg));
+      if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.startPlay(msg));
       if (queue[msg.guild.id].playing || queue[msg.guild.id].playing == undefined) return;
 
 
