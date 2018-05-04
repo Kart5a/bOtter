7743950dd96ca44442ecb6f7926d661ceff8aae6
Kart5a <kaartovuori.tino@gmail.com>
2018-05-04 18:47:01 +0300

    m

--- server.js
+++ server.js
@@ -56,15 +56,27 @@ function changeTitle(text) {
   });
 }
 
-function printSlot(_eka, _toka, _kolmas, _voitto, target_id, msg) {
+function printSlot(_eka, _toka, _kolmas, _voitto, target_id, msg, _panos) {
 
   ref.on('value', gotData, errData);
 
   var rahat = "" + data[target_id]["rahat"];
   var väli1 = " ".repeat(8 - rahat.length);
-  var str = "Rahat: " + rahat + väli1 + "|▇ " + _eka + " ▇▇ " + _toka + " ▇▇ " + _kolmas + " ▇|" + "\nVoitit: " + _voitto;
+  var str = "Rahat: " + rahat + ", Panos: " + _panos + "\n\n" + "|    -    " + _eka + "    -    " + _toka + "    -    " + _kolmas + "    -    |" + "\n\nVoitit: " + _voitto;
 
-  msg.channel.send(str);
+  msg.channel.send({
+    "embed": {
+      "color": 15466496,
+      "author": {
+        "name": "SLOTTIPOTTI",
+        "icon_url": "https://ih1.redbubble.net/image.517537251.7910/flat,800x800,075,f.u2.jpg"
+      },
+      "fields": [{
+        "name": "***" + data[target_id]["name"] + "***",
+        "value": str
+      }]
+    }
+  });
 
 }
 
@@ -77,6 +89,7 @@ function printProfile(target_id, msg) {
   var kuvaus = data[target_id]["kuvaus"];
   var kuva = data[target_id]["kuva"];
   var rahat = data[target_id]["rahat"];
+  var aika = data[target_id]["aikakannuilla"];
 
   var avatar;
 
@@ -112,6 +125,10 @@ function printProfile(target_id, msg) {
           {
             "name": "***___Rahat:___***",
             "value": rahat
+          },
+          {
+            "name": "***___Aika kannulla:___***",
+            "value": aika + " min"
           }
         ]
       }
@@ -143,16 +160,108 @@ setInterval(function() {
     changeTitle("ttunes");
   }
 
+// antaa kaikille channelilla oleville rahaa 1/minuutti
+
+  var keyarr = client.channels.keyArray();
+  var v_channels = [];
+  for (var i of keyarr) {
+    var kan = client.channels.get(i);
+    if (kan.type == 'voice') {
+      var membrs = kan.members.keyArray();
+      for (var m of membrs) {
+        if (data[m]["rahat"] == undefined) {
+          data[m]["rahat"] = 0;
+        }
+        data[m]["rahat"] += 1;
+        if (data[m]["aikakannuilla"] == undefined) {
+          data[m]["aikakannuilla"] = 0;
+        }
+        data[m]["aikakannuilla"] += 1;
+      }
+    }
+  }
+  firebase.database().ref('profiles').set(data);
+
 }, 60000);
 
 
 const commands = {
-  'give': (msg) => {
+
+  'pelidata': (msg) => {
+
+    ref.on('value', gotData, errData);
+    let name = msg.content.split(' ')[1];
+
+    if (name == undefined || name == "") {
+      name = msg.author.id;
+    }
+    name = name.replace(/\D/g, '');
+    var target_id = name;
+
+    if (data[target_id]["pelit"] == undefined || data[target_id]["pelit"] == null) {
+      data[target_id]["pelit"] = {
+        "pelit": 0,
+        "voitot": 0,
+        "yhtsumma": 0,
+        "sasu": 0,
+        "karvis": 0,
+        "kys": 0,
+        "protect": 0,
+        "poggers1": 0,
+        "poggers2": 0,
+        "poggers3": 0,
+        "annetut" : 0,
+        "vastaanotetut" : 0
+      };
+    }
+
+    if (data[target_id]["rahat"] == null || data[target_id]["rahat"] == undefined) {
+      data[target_id]["rahat"] = 100;
+    }
+
+    var u;
+    var flag = false;
+    for (u in client.users.array()) {
+      var User = client.users.array()[u];
+      if (User.id == name) {
+        flag = true;
+      }
+    }
+    if (!flag) return msg.channel.sendMessage(`Kelvoton nimi.`);
+
+    var money = data[target_id]["rahat"];
+    var pelit = data[target_id]["pelit"]["pelit"];
+    var voitot = data[target_id]["pelit"]["voitot"];
+    var yht = data[target_id]["pelit"]["yhtsumma"];
+    var poggers3 = data[target_id]["pelit"]["poggers3"];
+    var poggers2 = data[target_id]["pelit"]["poggers2"];
+    var poggers1 = data[target_id]["pelit"]["poggers1"];
+    var karvis1 = data[target_id]["pelit"]["karvis"];
+    var sasu1 = data[target_id]["pelit"]["sasu"];
+    var kys1 = data[target_id]["pelit"]["kys"];
+    var protect1 = data[target_id]["pelit"]["protect"];
+    var vast = data[target_id]["pelit"]["vastaanotetut"];
+    var ann = data[target_id]["pelit"]["annetut"];
+
+    msg.channel.send("```Nimi: " + data[target_id]["name"] + "\nPelit: " + pelit + "\n" + "Voitetut pelit: " + voitot + "\n" + "Kaikki voitot: " + yht + "\n\n" + "Poggers x 3: " + poggers3 + "\n" + "Poggers x 2: " + poggers2 + "\n" + "Poggers x 1: " + poggers1 + "\n" + "Karvis: " + karvis1 + "\n" + "Sasu: " + sasu1 + "\n" + "Kys: " + kys1 + "\n" +"Protect: " + protect1 + "\n\nAnnetut rahet: " + ann + "\nVastaanotetut rahet: " + vast + "```");
+
+    firebase.database().ref('profiles').set(data);
+  },
+
+
+  'anna': (msg) => {
     let name = msg.content.split(' ')[1];
     let amount = msg.content.split(' ')[2];
 
+
     if ((name == '' || name === undefined)) return msg.channel.sendMessage(`Kirjoita !give ja summa`);
-    if (isNaN(amount)) return msg.channel.sendMessage(`Voit siirtää vain rahea, et muuta...`);
+    if (isNaN(amount)) return msg.channel.sendMessage(amount + ` ei voida antaa :D`);
+
+    if (parseInt(amount) < 0) {
+      return msg.channel.sendMessage("Älä saatana yritä viedä toisilta kädestä");
+    } else if (parseInt(amount) == 0) {
+      return msg.channel.sendMessage("Et voi siirtää et mitään...");
+    }
 
     name = name.replace(/\D/g, '');
 
@@ -172,30 +281,70 @@ const commands = {
     var target_id = name;
     var sender_id = msg.author.id;
 
+    if (data[target_id]["pelit"] == undefined || data[target_id]["pelit"] == null) {
+      data[target_id]["pelit"] = {
+        "pelit": 0,
+        "voitot": 0,
+        "yhtsumma": 0,
+        "sasu": 0,
+        "karvis": 0,
+        "kys": 0,
+        "protect": 0,
+        "poggers1": 0,
+        "poggers2": 0,
+        "poggers3": 0,
+        "annetut" : 0,
+        "vastaanotetut" : 0
+      };
+    }
+    if (data[sender_id]["pelit"] == undefined || data[sender_id]["pelit"] == null) {
+      data[sender_id]["pelit"] = {
+        "pelit": 0,
+        "voitot": 0,
+        "yhtsumma": 0,
+        "sasu": 0,
+        "karvis": 0,
+        "kys": 0,
+        "protect": 0,
+        "poggers1": 0,
+        "poggers2": 0,
+        "poggers3": 0,
+        "annetut" : 0,
+        "vastaanotetut" : 0
+      };
+    }
+
+    if (target_id == sender_id) return msg.channel.sendMessage(`Turhaa siirrät ittelles rahea...`);
+
     if (data[sender_id]["rahat"] == null || data[sender_id]["rahat"] == undefined) {
       data[sender_id]["rahat"] = 100;
     }
     if (data[sender_id]["rahat"] < parseInt(amount)) return msg.channel.sendMessage(`Sulla ei oo tarpeeks rahea...`);
     if (data[target_id]["rahat"] == null || data[target_id]["rahat"] == undefined) {
-      data[target_id]["rahat"] = 100;
+      data[target_id]["rahat"] = 300;
     }
 
     data[target_id]["rahat"] += parseInt(amount);
     data[sender_id]["rahat"] -= parseInt(amount);
+    data[target_id]["pelit"]["vastaanotetut"] += parseInt(amount);
+    data[sender_id]["pelit"]["annetut"] += parseInt(amount);
+
 
     firebase.database().ref('profiles').set(data);
     msg.channel.send("Rahet siirretty!");
 
   },
 
-  'money': (msg) => {
+  'rahat': (msg) => {
 
     let name = msg.content.split(' ')[1];
+
     ref.on('value', gotData, errData);
     var sender_id = msg.author.id;
 
     if (data[sender_id]["rahat"] == null || data[sender_id]["rahat"] == undefined) {
       data[sender_id]["rahat"] = 100;
+      firebase.database().ref('profiles').set(data);
     }
 
     if ((name == '' || name === undefined)) return msg.channel.sendMessage(`Sulla on rahaa ` + data[sender_id]["rahat"]);
@@ -224,23 +373,29 @@ const commands = {
   },
 
   'slot': (msg) => {
+
+    let panos = msg.content.split(' ')[1];
+    if ((panos == '' || panos === undefined)) {
+      panos = 10;
+    }
+    if (panos <= 0) return msg.channel.sendMessage(`Panos pitää olla positiivinen luku!`);
+    panos = Math.floor(panos);
+
     const karvis = msg.guild.emojis.find("name", "karvis");
     const sasu = msg.guild.emojis.find("name", "sasu");
     const protect = msg.guild.emojis.find("name", "protect");
     const poggers = msg.guild.emojis.find("name", "poggers");
     const kys = msg.guild.emojis.find("name", "kys2");
 
-
-
     ref.on('value', gotData, errData);
 
-    if (data[msg.author.id]["rahat"] < 10) return msg.channel.sendMessage(`Sulla ei oo varaa uhkapelata.`);
-    data[msg.author.id]["rahat"] -= 10;
+    if (data[msg.author.id]["rahat"] < panos) return msg.channel.sendMessage(`Sulla ei oo varaa uhkapelata.`);
+    data[msg.author.id]["rahat"] -= panos;
 
     var rulla = [];
     for (var i = 0; i < 3; i++) {
       var rnd = Math.floor(Math.random() * Math.floor(100 + 1));
-      if (rnd <= 40) {
+      if (rnd <= 30) {
         rulla.push(karvis);
 
       } else if (rnd <= 60) {
@@ -255,35 +410,93 @@ const commands = {
     }
 
     var voitto;
+    if (data[msg.author.id]["pelit"] == undefined || data[msg.author.id]["pelit"] == null) {
+      data[msg.author.id]["pelit"] = {
+        "pelit": 0,
+        "voitot": 0,
+        "yhtsumma": 0,
+        "sasu": 0,
+        "karvis": 0,
+        "kys": 0,
+        "protect": 0,
+        "poggers1": 0,
+        "poggers2": 0,
+        "poggers3": 0,
+        "annetut" : 0,
+        "vastaanotetut" : 0
+      };
+    }
+
     if (rulla[0] == poggers && rulla[1] == poggers && rulla[2] == poggers) {
-      voitto = 1000;
+      voitto = 100 * panos;
       data[msg.author.id]["rahat"] += voitto;
+      if (data[msg.author.id]["pelit"]["poggers3"] == undefined) {
+        data[msg.author.id]["pelit"]["poggers3"] = 0;
+      }
+      data[msg.author.id]["pelit"]["poggers3"] += 1;
     } else if (rulla[0] == poggers && rulla[1] == poggers) {
-      voitto = 30;
+      voitto = 5 * panos;
       data[msg.author.id]["rahat"] += voitto;
+      if (data[msg.author.id]["pelit"]["poggesr2"] == undefined) {
+        data[msg.author.id]["pelit"]["poggers2"] = 0;
+      }
+      data[msg.author.id]["pelit"]["poggers2"] += 1;
       // poggers x 2
     } else if (rulla[0] == poggers) {
-      voitto = 10;
+      voitto = 1 * panos;
       data[msg.author.id]["rahat"] += voitto;
+      if (data[msg.author.id]["pelit"]["poggers1"] == undefined) {
+        data[msg.author.id]["pelit"]["poggers1"] = 0;
+      }
+      data[msg.author.id]["pelit"]["poggers1"] += 1;
       // poggers x 1
     } else if (rulla[0] == rulla[1] && rulla[0] == rulla[2] && rulla[1] == rulla[2]) {
 
-        if (rulla[0] == kys) {
-          voitto = 40;
-          data[msg.author.id]["rahat"] += voitto;
-        } else if (rulla[0] == karvis || rulla[0] == sasu) {
-          voitto = 100;
-          data[msg.author.id]["rahat"] += voitto;
-        } else if (rulla[0] == protect) {
-          voitto = 60;
-          data[msg.author.id]["rahat"] += voitto;
+      if (rulla[0] == kys) {
+        voitto = 6 * panos;
+        data[msg.author.id]["rahat"] += voitto;
+
+        if (data[msg.author.id]["pelit"]["kys"] == undefined) {
+          data[msg.author.id]["pelit"]["kys"] = 0;
         }
-      } else {
+
+        data[msg.author.id]["pelit"]["kys"] += 1;
+      } else if (rulla[0] == karvis || rulla[0] == sasu) {
+        voitto = 12 * panos;
+        data[msg.author.id]["rahat"] += voitto;
+        if (rulla[0] == sasu) {
+          if (data[msg.author.id]["pelit"]["sasu"] == undefined) {
+            data[msg.author.id]["pelit"]["sasu"] = 0;
+          }
+          data[msg.author.id]["pelit"]["sasu"] += 1;
+        } else {
+          if (data[msg.author.id]["pelit"]["karvis"] == undefined) {
+            data[msg.author.id]["pelit"]["karvis"] = 0;
+          }
+          data[msg.author.id]["pelit"]["karvis"] += 1;
+        }
+      } else if (rulla[0] == protect) {
+        voitto = 20 * panos;
+        data[msg.author.id]["rahat"] += voitto;
+        if (data[msg.author.id]["pelit"]["protect"] == undefined) {
+          data[msg.author.id]["pelit"]["protect"] = 0;
+        }
+        data[msg.author.id]["pelit"]["protect"] += 1;
+      }
+    } else {
       voitto = 0;
     }
 
+    if (voitto > 0) {
+      data[msg.author.id]["pelit"]["yhtsumma"] += voitto;
+      data[msg.author.id]["pelit"]["voitot"] += 1;
+      data[msg.author.id]["pelit"]["pelit"] += 1;
+    } else {
+      data[msg.author.id]["pelit"]["pelit"] += 1;
+    }
+
     firebase.database().ref('profiles').set(data);
-    printSlot(rulla[0], rulla[1], rulla[2], voitto, msg.author.id, msg);
+    printSlot(rulla[0], rulla[1], rulla[2], voitto, msg.author.id, msg, panos);
 
 
   },
@@ -300,13 +513,13 @@ const commands = {
     }
 
   },
-  'wealthiest': (msg) => {
+  'rikkaimmat': (msg) => {
     ref.on('value', gotData, errData);
 
     var w_l = {};
     for (var id in data) {
       if (data[id]["rahat"] == null || data[id]["rahat"] == undefined) {
-        data[id]["rahat"] = 100;
+        data[id]["rahat"] = 300;
       }
       key = id;
       value = data[id]["rahat"];
@@ -340,7 +553,7 @@ const commands = {
     });
   },
 
-  'profiles': (msg) => {
+  'profiilit': (msg) => {
     ref.on('value', gotData, errData);
 
     var all_profiles = "";
@@ -358,7 +571,7 @@ const commands = {
   },
 
 
-  'profile': (msg) => {
+  'profiili': (msg) => {
 
     let name = msg.content.split(' ')[1];
     let category = msg.content.split(' ')[2];
@@ -407,7 +620,7 @@ const commands = {
           "motto": "Tyhjä",
           "kuvaus": "Tyhjä",
           "kuva": null,
-          "rahat": 100
+          "rahat": 300
         };
 
         data[target_id] = empty;
@@ -458,7 +671,7 @@ const commands = {
     }
   },
 
-  'join': (msg) => {
+  'liity': (msg) => {
     return new Promise((resolve, reject) => {
       voiceChannel = msg.member.voiceChannel;
       if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('En voinut liittyä voicekannulle...');
@@ -467,7 +680,7 @@ const commands = {
     });
   },
 
-  'play': (msg, manual = null, joining = false) => {
+  'soita': (msg, manual = null, joining = false) => {
 
     if (joining === true) {
       startPlay(msg);
@@ -548,7 +761,7 @@ const commands = {
   },
 
 
-  'queue': (msg) => {
+  'jono': (msg) => {
     if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Laita ttuneja jonoon: ${tokens.prefix}add`);
     let tosend = [];
     queue[msg.guild.id].songs.forEach((song, i) => {
@@ -556,11 +769,6 @@ const commands = {
     });
     msg.channel.sendMessage(`__**${msg.guild.name}, Musiikki jono:**__ Nyt **${tosend.length}** ttunea jonossa ${(tosend.length > 15 ? '*[Näyttää vain 15 viimeisintä]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
   },
-
-  'reboot': (msg) => {
-    if (msg.author.id == tokens.adminID) process.exit(); //Requires a node module like Forever to work.
-  },
-
   'pääpäivä': (msg) => {
     var pv = new Date();
     pvd = [pv.getDate(), pv.getMonth(), pv.getYear()];
@@ -684,11 +892,11 @@ const commands = {
 
         title: "**__Komennot:__**",
         fields: [{
-            name: tokens.prefix + "profile + jäbän username + (luo / nimi / motto / kuvaus / kuva)",
+            name: tokens.prefix + "profiili + jäbän username + (luo / nimi / motto / kuvaus / kuva)",
             value: "Näyttää käyttäjän profiilin. Suluissa olevilla voi muokata."
           },
           {
-            name: tokens.prefix + "profiles",
+            name: tokens.prefix + "profiilit",
             value: "Näyttää kaikki tarjolla olevat profiilit."
           },
           {
@@ -716,15 +924,15 @@ const commands = {
             value: "Heittää rahea"
           },
           {
-            name: tokens.prefix + "money + nimi",
+            name: tokens.prefix + "rahat + nimi",
             value: "Näyttää rahat"
           },
           {
-            name: tokens.prefix + "wealthiest",
+            name: tokens.prefix + "rikkaimmat",
             value: "Näyttää listan rikkaimmista."
           },
           {
-            name: tokens.prefix + "give + nimi",
+            name: tokens.prefix + "anna + nimi",
             value: "Antaa rahaasi henkilölle."
           },
           {
@@ -748,15 +956,15 @@ const commands = {
             value: "Tässä kaikki ttuneBottiin liittyvät komennot:"
           },
           {
-            name: tokens.prefix + "join",
+            name: tokens.prefix + "liity",
             value: "ttuneBotti tulee kannulle"
           },
           {
-            name: tokens.prefix + "play",
+            name: tokens.prefix + "soita",
             value: "Laittaa ttunen jonoon ja soittaa sen"
           },
           {
-            name: tokens.prefix + "queue",
+            name: tokens.prefix + "jono",
             value: "Näyttää ttunejonon"
           },
           {
@@ -772,7 +980,7 @@ const commands = {
             value: "Hheittää ttunen rroskiin"
           },
           {
-            name: tokens.prefix + "time",
+            name: tokens.prefix + "aika",
             value: "Näyttää biisin soitetun ajan"
           },
           {
@@ -814,7 +1022,7 @@ client.on('message', msg => {
 
   //REAGOI EMOTEJA VALITTUIHIN SANOIHIN
   reagoi([/homo/, /autisti/], ["sasu", "karvis"], msg);
-  reagoi([/kys/], ["kys2", "protect"], msg);
+  //reagoi([/kys/], ["kys2", "protect"], msg);
 
 
   if (!msg.content.startsWith(tokens.prefix)) return;
