017f1870b883a1b8bd1f401be5e6570a705b4411
bOtterData <43114356+bOtterData@users.noreply.github.com>
2020-02-01 14:25:44 +0200

    M

--- server.js
+++ server.js
@@ -761,6 +761,7 @@ function check_user_in_database(_id) {
                   .set(hash);
                 client.fetchUser(_id, false).then(u => {
                   u.send("Sun salasana botter.xyz sivustolle on ja se on: **" + pass + "**.\nVoit vaihtaa salasanan komennolla " + tokens.prefix + "salasana <uusi salasana>. Uuden salasanan pituus on oltava 6-18 merkkiä ja se saa sisaltää seuraavia merkkejä:\nabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+                  u.send("**!!! botter.xyz ei ole vielä toiminnassa !!!**");
                 });
               }
 
